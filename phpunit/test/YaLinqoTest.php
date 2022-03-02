3c0f7aa81ef84238c0410e3628ba0e3d4a30df17
SnowCait <SnowCait@users.noreply.github.com>
2022-03-03 04:15:34 +0900

    YaLinqo

---
+++ phpunit/test/YaLinqoTest.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class YaLinqoTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function select(): void
+    {
+        $xs = from(range(1, 10))
+            ->where(function($x) { return $x % 2 != 0; })
+            ->select(function($x) { return $x * $x; });
+        $this->assertSame([1, 9, 25, 49, 81], $xs->toList());
+    }
+}
