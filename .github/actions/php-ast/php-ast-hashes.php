bea6d8297eda2130db202c38aae2201dc626b07d
SnowCait <SnowCait@users.noreply.github.com>
2022-05-01 00:35:48 +0900

    output

--- .github/actions/php-ast/php-ast-hashes.php
+++ .github/actions/php-ast/php-ast-hashes.php
@@ -10,14 +10,14 @@ $file = $argv[2];
 
 $paths = json_decode(getenv('paths'), true);
 
-foreach($paths as $path) {
-    $hash = '';
-    if (file_exists($path)) {
-        $ast = ast\parse_file($path, $version);
-        $hash = md5(ast_dump($ast));
-    }
-    file_put_contents($file, "{$hash},{$path}\n", FILE_APPEND);
-}
+// foreach($paths as $path) {
+//     $hash = '';
+//     if (file_exists($path)) {
+//         $ast = ast\parse_file($path, $version);
+//         $hash = md5(ast_dump($ast));
+//     }
+//     file_put_contents($file, "{$hash},{$path}\n", FILE_APPEND);
+// }
 
 $hashes = array_map(function (string $path) use ($version): array {
     $hash = '';
