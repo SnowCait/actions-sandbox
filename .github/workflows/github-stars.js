d4903852da37bcf4c2dd1995de9732af08f711c7
雪猫 <SnowCait@users.noreply.github.com>
2022-07-17 04:49:13 +0900

    Update github-stars.js

--- .github/workflows/github-stars.js
+++ .github/workflows/github-stars.js
@@ -1,4 +1,5 @@
 module.exports = async ({github}) => {
+  // Target repositories
   const starredRepos = await github.rest.search.repos({
     q: 'stars:>100000',
     sort: 'stars',
@@ -6,4 +7,15 @@ module.exports = async ({github}) => {
   });
   console.log(starredRepos.data.total_count, starredRepos.data.incomplete_results);
   console.log(starredRepos.data.items.map(x => x.full_name));
+
+  // Each info
+  const repositoryNames = starredRepos.data.items;
+  repositoryNames.forEach(async repository => {
+    const content = await github.rest.repos.getContent({
+      owner: repository.owner.login,
+      repo: repository.name,
+      path: '/',
+    });
+    console.log(content.data);
+  });
 }
